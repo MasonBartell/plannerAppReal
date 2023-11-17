1b8d20ea65851e1f081a29c8012cf913cdfd49a6
Mason Bartell <masonjbartell@gmail.com>
2023-11-17 09:48:31 -0600

    delete

--- plannerApp/ViewController.swift
+++ plannerApp/ViewController.swift
@@ -6,36 +6,41 @@
 //
 
 import UIKit
-
+//Satic Vars
 class AppData {
     static var title = [String]()
     static var date = [String]()
-    
-    
-    
 }
 
 class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    let defaults = UserDefaults.standard
     
     
     @IBOutlet weak var tableViewOutlet: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         tableViewOutlet.dataSource = self
         tableViewOutlet.delegate = self
     }
+    
+    
+    
     override func viewDidAppear(_ animated: Bool) {
         tableViewOutlet.reloadData()
     }
+    
+    
     @IBAction func addAction(_ sender: UIButton) {
       performSegue(withIdentifier: "Addsegue", sender: self)
     }
+    
+    
+    
    
     @IBAction func saveButton(_ sender: UIBarButtonItem) {
-        
-        
-        
     }
     
     
@@ -49,7 +54,17 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         cell.detailTextLabel?.text = AppData.date[indexPath.row]
                return cell
     }
-
+   
+    
+    
+    //Delete
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            AppData.title.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .top)
+            
+        }
+    }
     
     
     
